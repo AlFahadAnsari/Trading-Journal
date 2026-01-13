2ddae5457493d268148a0e5056909a1eb87d9ff7
alfahad ansari <157451286+AlFahadAnsari@users.noreply.github.com>
2026-01-13 15:53:59 +0530

    done edit

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,19 @@
-
+import { PrismaClient } from '@prisma/client/edge'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from './generated/prisma/client'
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+  prisma: PrismaClient | undefined
 }
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL!,
 })
 
-export const db = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
