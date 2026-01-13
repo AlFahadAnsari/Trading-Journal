fa8c173320d7007cd5f48ec58244ca85f9bcd706
alfahad ansari <157451286+AlFahadAnsari@users.noreply.github.com>
2026-01-13 13:50:02 +0530

    issue on production

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,19 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from './generated/prisma/client'
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+    prisma: PrismaClient
+}
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+  connectionString: process.env.DATABASE_URL,
+})
 
-export const db =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+export const db = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
-export default db;
+export default db
