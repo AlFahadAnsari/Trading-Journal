c85f4279c4f21683536925506443f4d469e193c4
alfahad ansari <157451286+AlFahadAnsari@users.noreply.github.com>
2026-01-13 13:47:36 +0530

    changes final

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,20 @@
-
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from './generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+  prisma: PrismaClient | undefined;
+};
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const db = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
 
-export default db
+export default db;
